be5b1cae9b012d44e78b888b6c7bd08c59abb57c
JonaKl <147399846+JonaKl@users.noreply.github.com>
2023-10-23 11:30:51 +0200

    Aufgaben Einführung Arrays erledigt

---
+++ src/Arrays/Einfuehrung.java
@@ -0,0 +1,36 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Einfuehrung {
+    public static void main(String[] args) {
+        System.out.println(mittelwert(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(Arrays.toString(getBuchwerteAbschreibung(15000.0, 0.4, 100)));
+    }
+    //Aufgabe 1,3
+    static double mittelwert(int[] arr){
+        double  mittelwert = 0;
+        for (int j : arr) {
+            mittelwert += j;
+        }
+        return mittelwert / arr.length;
+    }
+    //Aufgabe 2
+    static void indexOutOfBoundsException(int[] arr){
+        System.out.println(arr[arr.length]);
+    }
+    //Aufgabe 4
+    static double[] getBuchwerteAbschreibung(double b, double p, int r){
+        int i;
+        double b2 = b;
+        for (i=0; b>=r; i++){
+            b=b*(1-p);
+        }
+        double[] ret = new double[i];
+        for (int j = 0; j < i; j++) {
+            ret[j] = b2;
+            b2 = b2*(1-p);
+        }
+        return ret;
+    }
+}
