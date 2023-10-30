624081c68851167fcd036dce2e9a78b8a142400c
JonaKl <147399846+JonaKl@users.noreply.github.com>
2023-10-30 12:29:15 +0100

    runtime logged

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -1,12 +1,28 @@
 // https://www.baeldung.com/java-quicksort
 
+import java.sql.Time;
+
+
 public class Quicksort {
     static int swaps = 0;
     public static void main(String[] args) {
+        Time time = new Time(System.currentTimeMillis());
         int[] toSort = { 9, -3, 5, 2, 6, 8, -6, 1, 3 };
-        // int[] toSort = { 9, -3, 5, 2, 6, 8, -6, 62, 60 };
         quickSort(toSort, 0, toSort.length - 1);
         System.out.println("swaps: " + swaps);
+        System.out.println("The algorithm needs " + (System.currentTimeMillis()-time.getTime()) + " ms to sort the array of the length " + toSort.length + ".");
+
+        time = new Time(System.currentTimeMillis());
+        toSort = new int[]{9, -3, 5, 2};
+        quickSort(toSort, 0, toSort.length - 1);
+        System.out.println("swaps: " + swaps);
+        System.out.println("The algorithm needs " + (System.currentTimeMillis()-time.getTime()) + " ms to sort the array of the length " + toSort.length + ".");
+
+        time = new Time(System.currentTimeMillis());
+        toSort = new int[]{9, -3, 5, 2,3,5,6,600,12,45,45,7,-4,-45,-456,-45,45,2,8,2,1,9,4,5,6,776,456,4678,676,543,243,4,67,3,456,456,78,3,123,-6,45645,654,6};
+        quickSort(toSort, 0, toSort.length - 1);
+        System.out.println("swaps: " + swaps);
+        System.out.println("The algorithm needs " + (System.currentTimeMillis()-time.getTime()) + " ms to sort the array of the length " + toSort.length + ".");
     }
     public static void quickSort(int[] arr, int begin, int end) {
         if (begin < end) {
